611cc2308ce1f14393c20dc2251afb0f81befec5
lamees-nd <lameesn@equitick.com>
2024-07-05 15:26:22 +0300

    update database and add script

--- prisma/migrations/20240705122202_initial_migration/migration.sql
+++ prisma/migrations/20240705122202_initial_migration/migration.sql
@@ -19,8 +19,8 @@ CREATE TABLE `Account` (
     `provider` VARCHAR(191) NOT NULL,
     `providerAccountId` VARCHAR(191) NOT NULL,
     `refresh_token` TEXT NULL,
-    `refresh_token_expires_in` INTEGER NULL,
     `access_token` TEXT NULL,
+    `refresh_token_expires_in` INTEGER NULL,
     `expires_at` INTEGER NULL,
     `token_type` VARCHAR(191) NULL,
     `scope` VARCHAR(191) NULL,
@@ -56,10 +56,12 @@ CREATE TABLE `User` (
 
 -- CreateTable
 CREATE TABLE `VerificationToken` (
+    `id` VARCHAR(191) NOT NULL,
     `identifier` VARCHAR(191) NOT NULL,
     `token` VARCHAR(191) NOT NULL,
     `expires` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `VerificationToken_token_key`(`token`),
-    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
+    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`),
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
